d085fa57d6422a082f6873247f4376844204ebfd
Maarten vd Meer <maartenvdlake@gmail.com>
2025-11-16 21:45:37 +0100

    partial setup

--- blender/scripts/main.py
+++ blender/scripts/main.py
@@ -1,1 +1,31 @@
-trial
+import sys, os
+
+base_path = r"C:\sea_models\unity-sea_photography"
+script_path = os.path.join(base_path, "blender", "scripts")
+
+if script_path not in sys.path:
+    sys.path.append(script_path)
+
+import bpy
+import time
+
+import cleanup
+import importlib
+
+for module in [cleanup]:
+    importlib.reload(module)
+
+def main():
+    for module in [cleanup]:
+        importlib.reload(module)
+
+        # Set names to protect from cleanup
+    cleanup.bulk_cleanup(prefixes=(
+        "Cube",
+    ), protected_names = ("Tomato_LowPoly", "Tomato", "Crown", "Arm_L", "Arm_R", "Crown_LowPoly")
+    )
+
+
+# === run main only if in Blender context ===
+if __name__ == "__main__" or bpy.context.space_data is not None:
+    main()
